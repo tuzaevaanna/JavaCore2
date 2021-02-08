e80c48387e8c4b7f554a362240887461de40acb1
Tuzaeva Anna <tuzaeva.anna@mail.ru>
2021-02-08 13:09:13 +0300

    1

---
+++ src/lesson3/Apple.java
@@ -0,0 +1,14 @@
+package lesson3;
+
+public class Apple extends Fruit {
+
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "яблоки";
+    }
+}
+
---
+++ src/lesson3/Orange.java
@@ -0,0 +1,15 @@
+package lesson3;
+
+    public class Orange extends Fruit {
+
+        Orange() {
+            super(1.5f);
+        }
+
+        @Override
+        public String toString(){
+            return "апельсины";
+        }
+
+    }
+
